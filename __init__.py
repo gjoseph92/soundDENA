9e79ca6c64fbe719c9569a04ffa76c751d125157
Gabe Joseph <gabriel_joseph@nps.gov>
2015-10-05 16:16:58 -0800

    Expose Accessor class in top soundDB namespace

--- __init__.py
+++ __init__.py
@@ -2,16 +2,15 @@ from .metadata import fullMetadata, metadata
 from . import paths
 # from info import __doc__
 
-from . import accessor
+from .accessor import Accessor
 from .accessors import accessorExports
 
-accessors = { name: accessor.Accessor(parserFunc, pathToData) for name, (parserFunc, pathToData) in accessorExports.items() }
+accessors = { name: Accessor(parserFunc, pathToData) for name, (parserFunc, pathToData) in accessorExports.items() }
 
 globals().update( accessors )
 
-__all__ = list(accessors.keys()) + ["fullMetadata", "metadata", "paths"]
+__all__ = list(accessors.keys()) + ["fullMetadata", "metadata", "paths", "Accessor"]
 
 # clean up exported namespace
 del accessors
 del accessorExports
-del accessor
