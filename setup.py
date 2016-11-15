9e2a42c4796f5d86cf9619ebede35fa2aba356b2
Gabe Joseph <gjoseph92@gmail.com>
2016-11-14 19:07:29 -0700

    Change package name to soundDENA to avoid conflict with the new soundDB, which usurps this one

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name= "soundDB",
+    name= "soundDENA",
     version= "0.0.1",
-    description= "A tool for accessing the NSNSD hierarchical file structure like a queryable database",
+    description= "A tool for accessing the DENA natural sounds hierarchical file structure like a queryable database",
     url= "https://github.com/gjoseph92/soundDB",
     author= "Gabe Joseph",
     author_email= "gjoseph92@gmail.com",
